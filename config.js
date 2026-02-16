aedfd5b11312aa5da1353e163e371446c82a298f
Im-Dark0 <ghaidan9salah@gmail.com>
2026-02-16 15:19:44 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botClientID: "1471321751471394961",
+    botPrefix: "!",
+    ownerID: "1132572464589852673",
+    embedColor: "#00dbff",
+    embedfooterText: "Discord.js v14 Example by @Buddier"
+}
